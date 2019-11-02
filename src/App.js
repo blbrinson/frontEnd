500d75ffb4b5bca6a478b13ca723756e432a57cc
Brice Brinson <blbrinson@icloud.com>
2019-11-02 13:03:01 -0500

    fetched todos from an api, looped through them and displayed them as a list.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+constructor(props) {
+        super(props);
+        this.state = {
+          todoList: []
+        };
+      }
+  async componentDidMount() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/todos")
+    let todos = await response.json()
+    this.setState({todoList: todos})
+  }
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <ul>
+      		{this.state.todoList.map((todo) => (
+	      		<li>{todo.title}</li>
+      		))}      
+      </ul>
+      <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      </p>
+      <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+      >
           Learn React
         </a>
       </header>
     </div>
-  );
+    );
+  };
 }
 
 export default App;
